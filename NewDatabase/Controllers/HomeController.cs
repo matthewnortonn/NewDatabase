a8348d7a2c1a509133cd19571401865a2ca3ba6d
Matthew Norton <matthew.lee.norton@gmail.com>
2023-02-23 21:54:30 -0700

    Have the quadrant/edit and update and delete stuff added.Add project files.

---
+++ NewDatabase/Controllers/HomeController.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using NewDatabase.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewDatabase
+{
+    public class HomeController : Controller
+    {
+        private Context Context { get; set; }
+
+        public HomeController(Context logger)
+        {
+            Context = logger;
+        }
+
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        //task form GET
+        [HttpGet]
+        public IActionResult AddEdit()
+        {
+            ViewBag.Categorys = Context.Categorys.ToList();
+
+            return View("AddEdit", new ApplicationResponse());
+        }
+
+
+        //vew for the quadrant page
+        [HttpGet]
+        public IActionResult Quadrant()
+        {
+            var applications = Context.Responses.Include(x => x.Category).ToList();
+            ViewBag.Categorys = Context.Categorys.ToList();
+            return View("Quadrant", applications);
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+
+            ViewBag.Categorys = Context.Categorys.ToList();
+
+            var application = Context.Responses.Single(x => x.TaskId == id);
+            return View("AddEdit", application);
+        }
+        //task form post request to add new movies to the list
+        [HttpPost]
+        public IActionResult Edit(ApplicationResponse mr)
+        {
+
+            if (ModelState.IsValid)
+            {
+                Context.Update(mr);
+                Context.SaveChanges();
+                return RedirectToAction("Quadrant");
+            }
+            else
+            {
+                return View(mr);
+            }
+
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var application = Context.Responses.Single(x => x.TaskId == id);
+            Context.Responses.Remove(application);
+            Context.SaveChanges();
+
+            return RedirectToAction("Quadrant");
+        }
+        public IActionResult AddEdit(ApplicationResponse mr)
+        {
+            if (ModelState.IsValid)
+            {
+                Context.Add(mr);
+                Context.SaveChanges();
+
+                return View("Confirmation", mr);
+            }
+            else //invalid
+            {
+                ViewBag.Categorys = Context.Categorys.ToList();
+
+                return View(mr);
+            }
+
+
+        }
+
+
+
+
+
+
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
---
+++ NewDatabase/Models/Context.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace NewDatabase.Models
+{
+    public class Context : DbContext
+    {
+        //Constructor
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+        public DbSet<ApplicationResponse> Responses { get; set; }
+        public DbSet<Category> Categorys { get; set; }
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+           mb.Entity<Category>().HasData(
+              new Category { CategoryId = 1, CategoryName="Home"},
+               new Category { CategoryId = 2, CategoryName = "School" },
+               new Category { CategoryId = 3, CategoryName = "Work" },
+               new Category { CategoryId = 4, CategoryName = "Church" }
+               );
+
+        
+        }
+    }
+}
